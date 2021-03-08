67e64247f4691fa0340c22a08d3fa68f01f3df43
MB7 <beknazarmb@mail.ru>
2021-03-08 18:13:10 +0600

    Регистрация в админке

--- products/products_app/admin.py
+++ products/products_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from products_app.models import Products
+
 
 # Register your models here.
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'cost', 'remainder']
+    list_filter = ['name']
+    search_fields = ['category', 'cost']
+    fields = ['id', 'name', 'text', 'category', 'remainder', 'cost']
+    readonly_fields = ['id']
+
+
+admin.site.register(Products, ProductsAdmin)
+
